72da65ec74cc20a4cd4e5a7b4b717bd6e8e0024e
Moeru Ookami <moeru_ookami@hotmail.com>
2019-03-09 12:07:42 -0800

    Demo built in gradient blend vs gamma blend

--- src/ShinyPixel/MainWindow.xaml.cs
+++ src/ShinyPixel/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -15,6 +16,31 @@ namespace ShinyPixel
         public ObservableCollection<Line> Lines { get; set; }
         private Canvas _canvas;
 
+        private byte AverageColorBytes(byte first, byte second, double blend)
+        {
+            if ((blend > 1.0) || (blend < 0.0))
+            {
+                throw new ArgumentException();
+            }
+
+            double a = Convert.ToDouble(first);
+            double b = Convert.ToDouble(second);
+
+            return Convert.ToByte(Math.Sqrt((a * a) * blend + (b * b) * (1.0 - blend)));
+        }
+
+        private Color Blend(Color first, Color second, double blend)
+        {
+            return new Color
+            {
+                R = AverageColorBytes(first.R, second.R, blend),
+                G = AverageColorBytes(first.G, second.G, blend),
+                B = AverageColorBytes(first.B, second.B, blend),
+                A = AverageColorBytes(first.A, second.A, blend)
+            };
+
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -22,20 +48,57 @@ namespace ShinyPixel
             _canvas = new Canvas()
             {
                 Name = "MyCanvas",
-                Background = new SolidColorBrush(Colors.Green)
+                Background = new SolidColorBrush(Colors.White)
             };
 
-            var line = new Line()
+            var gradientBrush = new LinearGradientBrush()
             {
-                X1 = 600,
-                Y1 = 200,
-                X2 = 30,
-                Y2 = 30,
-                Stroke = new SolidColorBrush(Colors.Black),
-                StrokeThickness = 4.0
+                StartPoint = new Point(0, 0.5),
+                EndPoint = new Point(1, 0.5)
             };
 
-            _canvas.Children.Add(line);
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.Red, 0.0));
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.Green, 1.0));
+
+
+            var secondGradientBrush = new LinearGradientBrush()
+            {
+                StartPoint = new Point(0, 0.5),
+                EndPoint = new Point(1, 0.5)
+            };
+
+            secondGradientBrush.GradientStops.Add(new GradientStop(Colors.Red, 0.0));
+            secondGradientBrush.GradientStops.Add(new GradientStop(Colors.Green, 1.0));
+
+            var steps = 10;
+            for (int i = 1; i <= steps; i++)
+            {
+                var progress = i / (steps + 1.0);
+                var blend = 1.0 - progress;
+
+                secondGradientBrush.GradientStops.Add(
+                    new GradientStop(
+                        Blend(Colors.Red, Colors.Green, blend),
+                        progress));
+            }
+
+            var rectangle = new Rectangle()
+            {
+                Width = 800,
+                Height = 200,
+                Fill = gradientBrush
+            };
+
+            var secondRectangle = new Rectangle()
+            {
+                Width = 800,
+                Height = 200,
+                Fill = secondGradientBrush
+            };
+
+            _canvas.Children.Add(rectangle);
+            _canvas.Children.Add(secondRectangle);
+            Canvas.SetTop(secondRectangle, 200);
 
             _canvas.AddHandler(MouseDownEvent, new MouseButtonEventHandler(Canvas_MouseDown));
 
